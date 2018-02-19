f5bf0ad0c215722165a8b9125b0f25519f429b5b
Cuong Nguyen <nguyentc@us.ibm.com>
2018-02-19 16:30:26 -0600

    Initial Commit

---
+++ issNotification/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  issNotification
+//
+//  Created by Cuong Nguyen on 2/19/18.
+//  Copyright © 2018 Cuong Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
